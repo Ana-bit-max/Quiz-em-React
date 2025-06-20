18238304a60ee6cd12b68c6debfbfb9cb2f93f14
AnaLuiza <nininha_alesilva@hotmail.com>
2025-06-20 15:16:11 -0300

    Iniciando o projeto

---
+++ ARQUIVOS/quiz/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <h1>Quiz App</h1>
+      <p>Welcome to the Quiz App!</p>
+    </div>
+  );
+}
+
+export default App
