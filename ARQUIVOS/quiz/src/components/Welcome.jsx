150ae33caed63576c1a1ca8b9f2e5fc0e79fea75
AnaLuiza <nininha_alesilva@hotmail.com>
2025-06-25 16:14:32 -0300

    fazendo as perguntas

--- ARQUIVOS/quiz/src/components/Welcome.jsx
+++ ARQUIVOS/quiz/src/components/Welcome.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from 'react';
+import { QuizContext } from "../context/quiz";
+
 import Quiz from "../img/quiz.svg";
 
 import "./Welcome.css";
-// Welcome.jsx
 
 const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div id='Welcome'>
-        <h2>Seja bem-vindo</h2>
-        <p>Clique no botão para começar</p>
-        <button>Iniciar</button>
-        <img src= {Quiz} alt="Inicio do Quiz" />
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão para começar</p>
+      <button onClick={() => dispatch({ type: "CHANGE_STATE" })}>Iniciar</button>
+      <img src={Quiz} alt="Início do Quiz" />
     </div>
   );
 };
 
-export default Welcome
+export default Welcome;
--- ARQUIVOS/quiz/src/main.jsx
+++ ARQUIVOS/quiz/src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {QuizProvider} from "./context/quiz";
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <QuizProvider>
+        <App />
+    </QuizProvider>
+     </React.StrictMode> 
+);
